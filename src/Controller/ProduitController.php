4de1cec75656057e06e801ad2b88747408a76af8
DuDuch23 <alexduduch77@gmail.com>
2025-01-08 10:50:01 +0100

    controller produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class ProduitController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/produit', name: 'app_produit')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('produit/index.html.twig', [
+            'controller_name' => 'ProduitController',
         ]);
     }
 }
